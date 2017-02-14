0cea5a8515e7770742c94d93032e88203f68de96
Hramatski <gramazski@mail.ru>
2017-02-14 15:01:28 +0300

    StAX builder added.

--- src/main/java/com/gramazski/xmlparsing/builder/impl/StAXBuilder.java
+++ src/main/java/com/gramazski/xmlparsing/builder/impl/StAXBuilder.java
@@ -1,16 +1,103 @@
 package com.gramazski.xmlparsing.builder.impl;
 
 import com.gramazski.xmlparsing.builder.AbstractXMLBuilder;
+import com.gramazski.xmlparsing.entity.descriptor.EntityStringDescriptor;
+import com.gramazski.xmlparsing.entity.hierarchy.Device;
+import com.gramazski.xmlparsing.exception.XMLBuildingException;
+import com.gramazski.xmlparsing.factory.manager.DeviceFactoriesManager;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 /**
  * Created by gs on 07.02.2017.
  */
 public class StAXBuilder extends AbstractXMLBuilder {
+    private XMLInputFactory inputFactory;
+    private DeviceFactoriesManager factoriesManager;
+
     public StAXBuilder(){
         super();
+        factoriesManager = new DeviceFactoriesManager();
+        inputFactory = XMLInputFactory.newInstance();
     }
 
-    public void buildDevices(String fileName) {
+    public void buildDevices(String fileName) throws XMLBuildingException {
+        FileInputStream inputStream = null;
+        XMLStreamReader reader = null;
+        try {
+            inputStream = new FileInputStream(new File(fileName));
+            reader = inputFactory.createXMLStreamReader(inputStream);
 
+            while (reader.hasNext()) {
+                int type = reader.next();
+                if (type == XMLStreamConstants.START_ELEMENT) {
+                     String name = reader.getLocalName();
+                    if (isValidElementType(name)) {
+                        Device device = buildStudent(reader);
+                        devices.addDevice(device);
+                    }
+                }
+            }
+        } catch (XMLStreamException ex) {
+            throw new XMLBuildingException("StAX parsing error: " + ex.getMessage(), ex);
+        } catch (FileNotFoundException ex) {
+            throw new XMLBuildingException("File " + fileName + " not found.", ex);
+        } finally {
+            try {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+            } catch (IOException e) {
+                //Should be logging
+            }
+        }
+    }
+
+    private Device buildStudent(XMLStreamReader reader) throws XMLStreamException {
+        EntityStringDescriptor deviceDescriptor = new EntityStringDescriptor(reader.getLocalName());
+        addElementAttributes(deviceDescriptor, reader);
+        String startName = reader.getLocalName();
+        while (reader.hasNext()) {
+            int type = reader.next();
+            switch (type) {
+                case XMLStreamConstants.START_ELEMENT:
+                    deviceDescriptor.addAttribute(reader.getLocalName(), getXMLText(reader));
+                    break;
+                case XMLStreamConstants.END_ELEMENT:
+                    if (startName.equals(reader.getLocalName())){
+                        Device device = factoriesManager.createDevice(deviceDescriptor);
+                        return device;
+                    }
+                    break;
+            }
+        }
+
+        throw new XMLStreamException("Unknown element in tag Student");
+    }
+
+    private String getXMLText(XMLStreamReader reader) throws XMLStreamException {
+        String text = null;
+        if (reader.hasNext()) {
+            reader.next();
+            text = reader.getText();
+        }
+        return text;
+    }
+
+    private void addElementAttributes(EntityStringDescriptor deviceDescriptor, XMLStreamReader reader){
+        for (int i = 0; i < reader.getAttributeCount(); i++){
+            deviceDescriptor.addAttribute(reader.getAttributeLocalName(i), reader.getAttributeValue(i));
+        }
+    }
+
+    private boolean isValidElementType(String elementName){
+        return ("cooler".equals(elementName)) || ("cpu".equals(elementName));
     }
 }
---
+++ src/test/java/com/gramazski/xmlparsing/builder/impl/StAXBuilderTest.java
@@ -0,0 +1,31 @@
+package com.gramazski.xmlparsing.builder.impl;
+
+import com.gramazski.xmlparsing.entity.Devices;
+import com.gramazski.xmlparsing.exception.XMLBuildingException;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by gs on 14.02.2017.
+ */
+public class StAXBuilderTest {
+    private static Devices devices;
+    @Before
+    public void init(){
+        try {
+            StAXBuilder staxBuilder = new StAXBuilder();
+            staxBuilder.buildDevices("src/main/resources/shema/devices.xml");
+            devices = staxBuilder.getDevices();
+        }
+        catch (XMLBuildingException ex){
+            Assert.fail(ex.getMessage());
+        }
+    }
+    @Test
+    public void buildDevices() throws Exception {
+        String actualName = "Third";
+        Assert.assertTrue("Build device test failed.", devices.getDevice().get(0).getName().equals(actualName));
+    }
+
+}
